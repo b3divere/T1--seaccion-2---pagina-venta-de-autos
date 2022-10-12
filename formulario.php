e5480691f8203a7118fc06da67c937abf0665419
Rodrigo <rgutierrezvielma@gmail.com>
2022-10-12 16:08:33 -0300

    actualizacion php

--- formulario.php
+++ formulario.php
@@ -2,7 +2,7 @@
 //Este programa PHP debe guardarse con el nombre:  acualizaPobla.php
 
 //Me conecto al motor de BD que corre en localhost, con el usuario root (sin password), y selecciono la BD del proyecto
- $bd = mysqli_connect("localhost","root", "","world");
+ $bd = mysqli_connect("localhost","root", "","tienda");
   
 //Recibo el porcentaje y el Id de categoría desde el formulario
   $rut  = $_GET["rut"];
@@ -12,7 +12,7 @@
   $ncontac = $_GET["ncontacto"];
   $corre = $_GET["correo"];
 //Realizo la consulta SQL (llamada al Proc Almacenado)
-$respuesta = mysqli_query($bd, "CALL ActualizaPoblacionPais($rut, $nom, $ape, $dire, $ncontac, $corre)");
+$respuesta = mysqli_query($bd, "CALL cliente_AI($rut, $nom, $ape, $dire, $ncontac, $corre)");
   
 
 //Muestro un mensaje si todo estuvo bien.
