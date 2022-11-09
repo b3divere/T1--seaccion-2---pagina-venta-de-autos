74509df92f5482cddc245a462f37c240657d265f
Matias <matiasignacio.arayam@gmail.com>
2022-11-09 00:24:04 -0300

    actualizacion de daos del php login

--- login-registro.php
+++ login-registro.php
@@ -1,38 +1,42 @@
 <?php
 include_once('db.php');
+$Usuar = $_POST['usuario'];
 $nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$usuario = $_POST['usuario'];
+$apell = $_POST['apellido'];
+$rut = $_POST['rut'];
+$direcc = $_POST['direc'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
 $contrasena = $_POST['contrasena'];
 
 $conectar=conn();
 
 // query o consulta
-$query = "INSERT INTO users(nombre, correo, usuario, contrasena)
-            VALUES('$nombre', '$correo', '$usuario', '$contrasena')";
+$query = "INSERT INTO registro_usuario(usuario, clave, rut, nombre, apellido, direccion, numerocontacto, correoelectronico)
+            VALUES('$Usuar', '$contrasena', '$rut', '$nombre', '$apell', '$direcc', '$telefono', '$email' )";
 
 // Verificar que el correo no se repita y ya este inscrito
 
-$verificar_correo = mysqli_query($conectar, "SELECT * FROM users WHERE correo='$correo' ");
+$verificar_correo = mysqli_query($conectar, "SELECT * FROM registro_usuario WHERE correoelectronico='$email' ");
 // si existe una fila con el mismo correo
 if(mysqli_num_rows($verificar_correo) > 0){
     ?>
     <script>
         alert("Este correo ya esta registrado, intenta con otro");
-        window.location = "login-register.html";
+        window.location = "registrar.html";
     </script>
     <?php
     exit();
 }
 
 // verifica que el usuario no se repita
-$verificar_usuario = mysqli_query($conectar, "SELECT * FROM users WHERE usuario='$usuario' ");
+$verificar_usuario = mysqli_query($conectar, "SELECT * FROM registro_usuario WHERE usuario='$Usuar' ");
 // si existe una fila con el mismo correo
 if(mysqli_num_rows($verificar_usuario) > 0){
     ?>
     <script>
         alert("Este usuario ya esta registrado, intenta con otro");
-        window.location = "login-register.php";
+        window.location = "registrar.html";
     </script>
     <?php
     exit();
@@ -44,14 +48,14 @@ $ejecutar = mysqli_query($conectar, $query);
 if ($ejecutar) {
     ?>
     <script>
-        window.location.replace("login-register.php");
+        window.location.replace("login.html");
         window.alert("El usuario se ha registrado correctamente");
     </script>
     <?php
 } else {
     ?>
     <script>
-        window.location.replace("login-register.php");
+        window.location.replace("registrar.html");
         window.alert("Ups, algo ha ocurrido, intenta de nuevo");
     </script>
     <?php
--- login_usuario.php
+++ login_usuario.php
@@ -3,27 +3,27 @@
 session_start();
 
 include_once('db.php');
-$usuar = $_POST['correo'];
+$corr = $_POST['email'];
 $contrasena = $_POST['contrasena'];
 
 // conectar con base de datos
 $conectar=conn();
 
 // query para validar si el correo usado esta en la base de datos
-$validar_login = mysqli_query($conectar, "SELECT * FROM users WHERE correo='$usuar' 
-and contrasena='$contrasena'");
+$validar_login = mysqli_query($conectar, "SELECT * FROM registro_usuario WHERE correoelectronico='$corr' 
+and clave='$contrasena'");
 
 // si encuentra algun usuario o correo que existe
 if(mysqli_num_rows($validar_login) > 0){
     // sesion iniciada
-    $_SESSION['usuario'] = $correo;
-    header("location: bienvenida.html");
+    $_SESSION['usuario'] = $corr;
+    header("location: index.html");
     exit();
 }else{
     ?>
     <script>
         alert("Usuario ingresado no existe, por favor verifique los datos introducidos");
-        window.location = "login-register.html";
+        window.location = "login.html";
     </script>
     <?php
     exit();
